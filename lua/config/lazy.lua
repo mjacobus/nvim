99433c9ee813d3edbe90f186d1307d6bdd1f54f8
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-06-20 13:41:04 -0300

    Revert "Install illuminate pluging"
    This reverts commit 87bbc5a9dcaf2283d20cf124eab1f34aabb404ef.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,9 +19,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.editor.refactoring" },
-    { import = "lazyvim.plugins.extras.editor.aerial" }, -- search buffer symbols with telescope <leader>ss
-    { import = "lazyvim.plugins.extras.editor.dial" }, -- like speed dating
-    { import = "lazyvim.plugins.extras.editor.illuminate" }, -- highlight word under cursor
+    { import = "lazyvim.plugins.extras.editor.aerial" },
+    { import = "lazyvim.plugins.extras.editor.dial" },
     -- import/override with your plugins
     { import = "plugins" },
   },
