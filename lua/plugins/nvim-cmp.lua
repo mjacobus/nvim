18edacd5f529e56397f4d3a7b1d70eca4b12e0df
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-06-21 11:05:28 -0300

    Remove code that does not work

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -14,13 +14,6 @@ return {
         completion = require("cmp").config.window.bordered(),
         documentation = require("cmp").config.window.bordered(),
       }
-      -- Set custom highlights to remove transparency
-      vim.api.nvim_set_hl(0, "Pmenu", { bg = "#1e222a", fg = "#c5cdd9" })
-      vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#3e4452", fg = "#c5cdd9" })
-      vim.api.nvim_set_hl(0, "CmpItemAbbr", { fg = "#c5cdd9" })
-      vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#569cd6" })
-      vim.api.nvim_set_hl(0, "CmpItemKind", { fg = "#56b6c2" })
-      vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#c5cdd9" })
     end,
   },
 }
