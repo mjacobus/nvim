3be0d6edebd219a9b660aeb0219c87728e87d2e1
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-06-21 11:28:28 -0300

    Disable noice plugin

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,8 +1,8 @@
 return {
   "folke/noice.nvim",
-  opts = {
-    cmdline = {
-      view = "cmdline",
-    },
-  },
+  enabled = false,
+  -- opts = {
+  --   cmdline = {
+  --     view = "cmdline", },
+  -- },
 }
