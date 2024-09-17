88a55e21bcd9dbfd6994aa21d3b73d1629b939bd
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-09-17 10:11:27 -0300

    Not sure this works. I will check later.
    Changed: after/ftplugin/ruby.lua

--- after/ftplugin/ruby.lua
+++ after/ftplugin/ruby.lua
@@ -1,10 +1,20 @@
-vim.cmd([[
-  autocmd FileType ruby nnoremap <buffer> <leader>cs :lua RunRubocop()<CR>
-]])
+local map = LazyVim.safe_keymap_set
 
-vim.cmd([[
-  autocmd FileType ruby nnoremap <buffer> <leader>x :lua RunInTerminal("ruby " .. vim.fn.expand('%'), true)<CR>
-]])
+-- map("n", "<leader>x", ":lua RunInTerminal('ruby ' .. vim.fn.expand('%'), true)<CR>", { buffer = true })
+
+print("haa")
+map("n", "<leader>x", function()
+  print("Running ruby file")
+  -- RunInTerminal("ruby " .. vim.fn.expand('%'), true)
+end, { desc = "Execute ruby file", buffer = true })
+
+-- vim.cmd([[
+--   autocmd FileType ruby nnoremap <buffer> <leader>cs :lua RunRubocop()<CR>
+-- ]])
+--
+-- vim.cmd([[
+--   autocmd FileType ruby nnoremap <buffer> <leader>x :lua RunInTerminal("ruby " .. vim.fn.expand('%'), true)<CR>
+-- ]])
 
 -- Maybe this
 -- vim.api.nvim_create_autocmd("FileType", {
