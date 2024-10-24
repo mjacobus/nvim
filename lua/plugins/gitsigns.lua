590a5883788396c75e68717a84dc5a36c113b25a
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-10-24 16:02:54 -0300

    Better gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,15 @@
+require("gitsigns").setup({
+  current_line_blame = false,
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    -- Stage hunk
+    -- vim.keymap.set("n", "<leader>ga", gs.stage_hunk, { buffer = bufnr })
+    vim.keymap.set("v", "<leader>ga", function()
+      gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+    end, { buffer = bufnr })
+
+    -- Stage buffer
+    vim.keymap.set("n", "<leader>ga", gs.stage_buffer, { buffer = bufnr })
+  end,
+})
