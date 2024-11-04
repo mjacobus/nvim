5078c097f80ff796ef5b37a492a7ad5fa27202cf
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-11-04 16:23:03 -0300

    Add mapping for OpenGithubFile

--- lua/plugins/open-browser-github.lua
+++ lua/plugins/open-browser-github.lua
@@ -1,4 +1,15 @@
 return {
   "tyru/open-browser-github.vim",
   dependencies = { { "tyru/open-browser.vim" } },
+  config = function()
+    local map = LazyVim.safe_keymap_set
+
+    map("n", "<leader>of", function()
+      vim.cmd("OpenGithubFile")
+    end, { desc = "Open Github file" })
+
+    map("v", "<leader>of", function()
+      vim.api.nvim_command("'<,'>OpenGithubFile")
+    end, { desc = "Open Github file (visual)" })
+  end,
 }
