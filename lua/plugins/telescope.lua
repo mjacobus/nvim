587307ca0962253171abdab79374a6d8d36c88b1
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-11-18 12:48:55 -0300

    Grep with arguments within telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,26 @@
 -- lua/plugins/telescope.lua
 -- https://www.lazyvim.org/extras/editor/telescope
 return {
-  "nvim-telescope/telescope.nvim",
-  opts = function(_, opts)
-    opts.keys = opts.keys or {}
-    opts.keys["<Space><Space>"] = function()
-      require("telescope.builtin").find_files({
-        cwd = require("project_nvim.project").get_project_root() or vim.fn.getcwd(),
-      })
-    end
-  end,
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+      opts.keys = opts.keys or {}
+      opts.keys["<Space><Space>"] = function()
+        require("telescope.builtin").find_files({
+          cwd = require("project_nvim.project").get_project_root() or vim.fn.getcwd(),
+        })
+      end
+    end,
+  },
+  {
+    "nvim-telescope/telescope-live-grep-args.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("telescope").load_extension("live_grep_args")
+
+      vim.keymap.set("n", "<leader>sg", function()
+        require("telescope").extensions.live_grep_args.live_grep_args()
+      end, { desc = "Live Grep with Args" })
+    end,
+  },
 }
