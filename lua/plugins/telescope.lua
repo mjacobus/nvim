f35d669229bacb49a4e3cf7584e000f5ad48f43f
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-12-18 11:49:53 -0300

    New live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,26 +10,10 @@ return {
           cwd = project_root, -- Force project root
         })
       end, { desc = "Find Files (Project Root)" })
-    end,
-  },
-  {
-    "nvim-telescope/telescope-live-grep-args.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" },
-    config = function()
-      require("telescope").load_extension("live_grep_args")
 
-      vim.keymap.set("n", "<leader>sg", function()
-        require("telescope").extensions.live_grep_args.live_grep_args()
-      end, { desc = "Live Grep with Args" })
+      vim.keymap.set("n", "<Space>sg", function()
+        require("mj.telescope.grep_with_args").setup()
+      end, { desc = "Custom Live Grep With args" })
     end,
   },
 }
-
--- opts = function(_, opts)
---     vim.keymap.set("n", "<Space><Space>", function()
---       local project_root = require("project_nvim.project").get_project_root() or vim.fn.getcwd()
---       require("telescope.builtin").find_files({
---         cwd = project_root, -- Force project root
---       })
---     end, { desc = "Find Files (Project Root)" })
---   end,
