5a929fd82fc759cc9a00ee19188d5716c9d293c2
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2025-01-16 17:13:52 -0300

    Remove debug code

--- after/ftplugin/ruby.lua
+++ after/ftplugin/ruby.lua
@@ -2,10 +2,8 @@ local map = LazyVim.safe_keymap_set
 
 -- map("n", "<leader>x", ":lua RunInTerminal('ruby ' .. vim.fn.expand('%'), true)<CR>", { buffer = true })
 
-print("haa")
 map("n", "<leader>x", function()
-  print("Running ruby file")
-  -- RunInTerminal("ruby " .. vim.fn.expand('%'), true)
+  RunInTerminal("ruby " .. vim.fn.expand("%"), true)
 end, { desc = "Execute ruby file", buffer = true })
 
 -- vim.cmd([[
