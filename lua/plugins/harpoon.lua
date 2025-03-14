8697939562547ff8eaed5f39ca84643d680e78cd
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2025-03-14 10:17:57 -0300

    Telescope harpoon files

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,6 +9,10 @@ return {
       save_on_toggle = true,
     },
   },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
   keys = function()
     local keys = {
       {
@@ -38,5 +42,34 @@ return {
       })
     end
     return keys
+  end,
+  config = function()
+    local harpoon = require("harpoon")
+    harpoon:setup({})
+
+    -- basic telescope configuration
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers")
+        .new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        })
+        :find()
+    end
+
+    local set = LazyVim.safe_keymap_set
+    set("n", "<C-e>", function()
+      toggle_telescope(harpoon:list())
+    end, { desc = "Open harpoon window" })
   end,
 }
