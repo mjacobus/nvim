b5ee1e5c47fbb629c0172656c37d6390ff1e1e2e
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2025-04-13 12:54:18 -0300

    Dart support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,14 @@
 return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
-    print("heeeey")
     opts.servers = opts.servers or {}
+
+    -- Ruby LSP
     opts.servers.ruby_lsp = vim.tbl_deep_extend("force", opts.servers.ruby_lsp or {}, {
       cmd = { vim.fn.expand("~/.asdf/shims/ruby"), "-S", "ruby-lsp" },
     })
+
+    -- Dart LSP (Flutter)
+    opts.servers.dartls = {}
   end,
 }
