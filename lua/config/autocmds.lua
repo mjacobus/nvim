b09193fbac9f4ada933e2f11268d460c5ee10b14
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2025-04-15 10:23:00 -0300

    Set .project.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local project_config = vim.fn.getcwd() .. "/.project.lua"
+    if vim.fn.filereadable(project_config) == 1 then
+      dofile(project_config)
+    end
+  end,
+})
