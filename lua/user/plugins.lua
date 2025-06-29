fbbd2eb9ec06abc3ef1b538453fe8e9c8cc0e528
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2025-06-29 20:30:19 -0300

    Install harpoon

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -8,10 +8,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  { "nvim-lua/plenary.nvim" },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  { "nvim-telescope/telescope.nvim", tag = "0.1.5" },
-  { "catppuccin/nvim", name = "catppuccin" },
-  { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {}, }
-})
+-- require("lazy").setup({
+--   { "nvim-lua/plenary.nvim" },
+--   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+--   { "nvim-telescope/telescope.nvim", tag = "0.1.5" },
+--   { "catppuccin/nvim", name = "catppuccin" },
+--   { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {}, }
+-- })
+
+require('lazy').setup('plugins') -- lua/plugins/*
