cafae26afeb701c7ac8564429d2cf51e88c0b8e0
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2025-07-01 11:10:43 -0300

    Fix tab on command line

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -136,17 +136,18 @@ return {
         },
       })
 
+      -- TODO: Fix this. Autocomplete messed up regular tab completion
       -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' }
-        }
-      })
+      -- cmp.setup.cmdline({ '/', '?' }, {
+      --   mapping = cmp.mapping.preset.cmdline(),
+      --   sources = {
+      --     { name = 'buffer' }
+      --   }
+      -- })
 
       -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 
-      cmp.setup.cmdline(':', {})
+      -- cmp.setup.cmdline(':', {})
       -- cmp.setup.cmdline(':', {
       --   mapping = cmp.mapping.preset.cmdline(),
       --   sources = cmp.config.sources({
