52a0e3d7db30e0421be86a9ac6b201042bc321e0
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2025-07-15 12:06:34 -0300

    Add undo

--- old/lua/config/options.lua
+++ old/lua/config/options.lua
@@ -8,3 +8,10 @@ local opt = vim.opt
 opt.relativenumber = false
 opt.colorcolumn = "100"
 opt.swapfile = false
+
+
+opt.undofile = true
+opt.undodir = vim.fn.stdpath("state") .. "/undo"
+
+-- Optional: make sure the directory exists
+vim.fn.mkdir(opt.undodir:get(), "p")
