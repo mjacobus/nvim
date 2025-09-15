6f43fd28be2550b9d5ed8d34cd2d96c3ec85e5ba
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2025-09-15 16:22:45 -0300

    Install mjvim

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 args=$@
 
-targetDir=$HOME/.mjvim
+targetDir=$HOME/.config/mjvim
 mkdir -p $targetDir
 flags="--verbose=1 -t ${targetDir}"
 
@@ -13,6 +13,6 @@ fi
 
 stow $flags . # newest
 
-# Create alias for starting nvim with custom config
-echo "alias mjvim='NVIM_APPNAME=.mjvim nvim'" >> ~/.bashrc
-echo "Alias 'mjvim' added to ~/.bashrc. Run 'source ~/.bashrc' or restart terminal to use."
+# # Create alias for starting nvim with custom config
+# echo "alias mjvim='NVIM_APPNAME=.mjvim nvim'" >> ~/.bashrc
+# echo "Alias 'mjvim' added to ~/.bashrc. Run 'source ~/.bashrc' or restart terminal to use."
