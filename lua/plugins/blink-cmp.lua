9f7e0545ea13521e4a446a35e767cbbd2b9657db
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2025-11-06 19:35:29 -0300

    Install lazydev

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -9,7 +9,18 @@ return {
       "L3MON4D3/LuaSnip",
       version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
       build = "make install_jsregexp"
-    }
+    },
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    },
   },
   version = '1.*',
   build = 'cargo build --release',
@@ -49,6 +60,7 @@ return {
     },
     sources = {
       default = {
+        'lazydev',
         'lsp',
         'path',
         'snippets',
@@ -60,6 +72,12 @@ return {
           name = "copilot",
           module = "blink-copilot",
           async = true,
+        },
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          -- make lazydev completions top priority (see `:h blink.cmp`)
+          score_offset = 100,
         },
       },
     },
