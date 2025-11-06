40b80878427d9a40877c9ff2dc75e27724f90ba3
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2025-11-06 11:14:26 -0300

    Update plugins, including neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,5 +1,11 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
+  branch = "v3.x",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
   keys = {
     { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle NeoTree" },
   },
