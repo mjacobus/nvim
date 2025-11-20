27409f4e75b56d4ed3880628c1f00a0eb6e0c422
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2025-11-20 13:59:25 -0300

    Disable annoying root thing for conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,20 +1,20 @@
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "sql",
-  callback = function()
-    local root = vim.fs.dirname(vim.fs.find({ ".git", "pyproject.toml" }, {
-      upward = true,
-      path = vim.api.nvim_buf_get_name(0),
-    })[1])
-
-    if root and not vim.loop.fs_stat(root .. "/.sqlfluff") then
-      vim.fn.writefile({
-        "[sqlfluff]",
-        "dialect = postgres",
-      }, root .. "/.sqlfluff")
-      print("Created .sqlfluff in " .. root)
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "sql",
+--   callback = function()
+--     local root = vim.fs.dirname(vim.fs.find({ ".git", "pyproject.toml" }, {
+--       upward = true,
+--       path = vim.api.nvim_buf_get_name(0),
+--     })[1])
+--
+--     if root and not vim.loop.fs_stat(root .. "/.sqlfluff") then
+--       vim.fn.writefile({
+--         "[sqlfluff]",
+--         "dialect = postgres",
+--       }, root .. "/.sqlfluff")
+--       print("Created .sqlfluff in " .. root)
+--     end
+--   end,
+-- })
 return {
   "stevearc/conform.nvim",
   event = { "BufReadPre", "BufNewFile" },
