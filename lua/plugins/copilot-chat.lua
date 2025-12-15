4a034b97963925586d2eb119b15453bea16f8f11
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2025-12-15 12:53:55 -0300

    Better

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "nvim-lua/plenary.nvim", branch = "master" },
-    },
-    build = "make tiktoken",
-    opts = {
-      -- See Configuration section for options
-    },
-  },
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   dependencies = {
+  --     { "nvim-lua/plenary.nvim", branch = "master" },
+  --   },
+  --   build = "make tiktoken",
+  --   opts = {
+  --     -- See Configuration section for options
+  --   },
+  -- },
 }
